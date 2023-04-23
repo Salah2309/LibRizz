607b012b9657e5dc17bc4e6cab10936b2cfeea11
Salah Alayoubi <Salahalayoubi23@gmail.com>
2023-04-22 20:24:16 -0400

    init comit :)

--- Runner.py
+++ Runner.py
@@ -1,3 +1,5 @@
+import datetime
+import pytz
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
@@ -16,36 +18,24 @@ url = "https://ucf.libcal.com/reserve/generalstudyroom"
 def main():
     driver = webdriver.Chrome()
     driver.get(url)
-    #time.sleep(10)
-    #time.sleep(2)
-    reservation_date=dt.datetime.now()+timedelta(days=8)
-    gotoday(driver, reservation_date)
-   
-    reserve(driver, (reservation_date.strftime("%B %d, %Y")), 'aa','a', "b")
-   # reserve(driver, "370", "12","1")
-    #<button class="fc-goToDate-button btn btn-default btn-sm" type="button" aria-label="Go To Date" data-original-title="" title=""><i class="fa fa-calendar" aria-class="hidden"></i> Go To Date</button>
-    #driver.find_element("name", "fa fa-calendar").click()
-    #driver.find_element("data-date", "1678665600000").click()
-    driver.implicitly_wait(90)
-    driver.implicitly_wait(90)
-    time.sleep(8)
-    time.sleep(15)
-	
+    driver = gotoday(driver,date_to_unix_timestamp('2023-04-18'))
+    time.sleep(25)
+
+
 def checkavailable(room, start_time, end_time):
 	
-
     return 0
 
+
 def gotoday(driver, day):
-	#setting time 0:0 in datetime 
-	epochdate = dt.datetime(int(day.strftime("%Y")), int(day.strftime("%m")), int(day.strftime("%d")), 0, 0, 0)
-	driver.find_element("xpath", "//button[@class='fc-goToDate-button btn btn-default btn-sm' and @aria-label='Go To Date']").click()
-	#converting datetime to epoch date
-	dvalue = calendar.timegm(epochdate.timetuple())*1000
-	driver.find_element("xpath", "//td[@data-date='"+ str(dvalue) +"']").click();
-	#time.sleep(3)
-	driver.implicitly_wait(4)
-	
+    driver.find_element("xpath","//button[@class='fc-goToDate-button btn btn-default btn-sm'and @aria-label='Go To Date']").click()
+    driver.find_element("xpath","//td[@data-date='1682121600000']").click()
+    return driver
+
+def date_to_unix_timestamp(date_string):
+    date_object = datetime.datetime.strptime(date_string, '%Y-%m-%d').replace(tzinfo=pytz.timezone('GMT'))
+    return (int(date_object.timestamp())) *1000
+
 
 def reserve(driver, day, room, start_time, end_time):
 	#searches for room 
@@ -65,17 +55,50 @@ def reserve(driver, day, room, start_time, end_time):
 	driver.find_element("xpath", "//button[@type='submit' and @id='btn-form-submit']").click()
 	time.sleep(10)
 	
-	
+
 def login(driver, username, password):
- 
+    try:
+        username_field = driver.find_element_by_id("userNameInput")
+        password_field = driver.find_element_by_id("passwordInput")
+        sign_on_button = driver.find_element_by_id("submitButton")
+
+        time.sleep(20)
+        
         username_field = driver.find_element("xpath", "//input[@id='userNameInput']")
         username_field.send_keys(username)
         password_field = driver.find_element("xpath", "//input[@id='passwordInput']")
         password_field.send_keys(password)
         sign_on_button = driver.find_element("xpath", "//span[@id='submitButton']")
         sign_on_button.click()
-        time.sleep(5)
-        print("tried ")
-   
+    except:
+        print("Could Not Login!!!")
 
 main()
+
+#CHATGPT SAID:
+# def make_reservation():
+#     username = "your_username"
+#     password = "your_password"
+#     capacity = "10"
+#     time_slot = "12:00 PM - 1:00 PM"
+#     driver = webdriver.Chrome()
+#     driver.get(url)
+#     username_field = driver.find_element_by_id("username")
+#     username_field.send_keys(username)
+#     password_field = driver.find_element_by_id("password")
+#     password_field.send_keys(password)
+#     login_button = driver.find_element_by_name("_eventId_proceed")
+#     login_button.click()
+#     capacity_field = driver.find_element_by_id("capacity")
+#     capacity_field.send_keys(capacity)
+#     time_slot_field = driver.find_element_by_id("s-lc-rm-time")
+#     time_slot_field.send_keys(time_slot)
+#     submit_button = driver.find_element_by_name("s-lc-rm-submit")
+#     submit_button.click()
+#     driver.quit()
+
+# schedule.every().day.at("11:45").do(make_reservation)
+
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
